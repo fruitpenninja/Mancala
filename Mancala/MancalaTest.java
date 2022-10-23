910872006b1258a1390de5b8bc96b0c6a47fe6e1
Quang Le <65510865+quanghuyle3@users.noreply.github.com>
2022-10-22 22:18:36 -0700

    update

--- Mancala/MancalaTest.java
+++ Mancala/MancalaTest.java
@@ -5,6 +5,5 @@ public class MancalaTest {
 	public static void main(String[] args) {
 		System.out.println("Test print out!");
 		System.out.println("Test 2");
-		System.out.println("Test 3");
 	}
 }
