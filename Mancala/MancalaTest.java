61749336e9c659d96443bdaa40667f0f6c509663
Quang Le <quanghuyle3@gmail.com>
2022-10-22 22:17:23 -0700

    Commit #3

--- Mancala/MancalaTest.java
+++ Mancala/MancalaTest.java
@@ -5,5 +5,6 @@ public class MancalaTest {
 	public static void main(String[] args) {
 		System.out.println("Test print out!");
 		System.out.println("Test 2");
+		System.out.println("Test 3");
 	}
 }
