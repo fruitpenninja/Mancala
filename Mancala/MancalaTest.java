201bfaf3d62acada829fe382597fa4a3a61060a3
Quang Le <quanghuyle3@gmail.com>
2022-11-03 21:13:19 -0700

    Adding Stones to Pits

--- Mancala/MancalaTest.java
+++ Mancala/MancalaTest.java
@@ -7,7 +7,7 @@ public class MancalaTest {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setSize(400, 400);
-		MancalaBoard newGame = new MancalaBoard(frame.getWidth(), frame.getHeight());
+		MancalaBoard newGame = new MancalaBoard(frame.getWidth(), frame.getHeight(), 3);
 		frame.setLayout(new BorderLayout());
 		frame.add(newGame, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
